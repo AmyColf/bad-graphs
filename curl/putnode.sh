51f9a96626cbd045a605a3b8dbbfbd14c75e9411
Amy Colford <amy@zeitspace.com>
2019-05-28 16:09:32 -0400

    Update putnode to add two nodes.

--- curl/putnode.sh
+++ curl/putnode.sh
@@ -7,3 +7,11 @@ curl -X PUT \
   -d '{
 	"name": "Amy!"
 }'
+curl -X PUT \
+  http://localhost:4040/nodes \
+  -H 'Content-Type: application/json' \
+  -H 'Postman-Token: a7cbf776-43db-437f-9c7f-fbfd1b41fde6' \
+  -H 'cache-control: no-cache' \
+  -d '{
+	"name": "Not Amy!"
+}'
